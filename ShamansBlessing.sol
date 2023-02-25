69bc4d5238fd2317537406cff6f5831776c5049f
ShamanBlackout <47762410+ShamanBlackout@users.noreply.github.com>
2023-02-25 15:43:15 -0600

    Update ShamansBlessing.sol
    init  commit

--- ShamansBlessing.sol
+++ ShamansBlessing.sol
@@ -1,1 +1,22 @@
 
+// SPDX-License-Identifier: MIT
+/**
+ *Submitted for verification at Etherscan.io on 2020-10-19
+*/
+
+pragma solidity ^0.8.18;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+
+contract ShamansBlessing is ERC20,ERC20PresetFixedSupply {
+
+    uint256 private _initSupply = 500**decimals();
+    
+
+    constructor() ERC20PresetFixedSupply("ShamansBlessing","SMB",_initSupply,msg.sender) 
+    {
+    }
+
+
+    }
